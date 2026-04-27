eff483d4535166f2f70f75234643f73658695905
srikanth-perisync <srikanth@perisync.com>
2026-04-27 17:07:11 +0530

    fix: add routes/controllers/services for title-richtext and title-and-repeater

---
+++ src/api/title-and-repeater/controllers/title-and-repeater.js
@@ -0,0 +1,1 @@
+module.exports = require('@strapi/strapi').factories.createCoreController('api::title-and-repeater.title-and-repeater');
---
+++ src/api/title-and-repeater/routes/title-and-repeater.js
@@ -0,0 +1,1 @@
+module.exports = require('@strapi/strapi').factories.createCoreRouter('api::title-and-repeater.title-and-repeater');
